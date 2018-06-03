a068a88c5f3ae032c802133e3f22257aa531cc43
Divek <patel.divek@gmail.com>
2018-06-03 19:32:26 -0400

    Hello

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -5,15 +5,22 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.robotcore.internal.android.dex.Leb128;
+
 @TeleOp(name = "Robot", group = "TeleOp")
 public class Robot extends LinearOpMode {
-    DcMotor Left;
-    DcMotor Right;
+    DcMotor L1;
+    DcMotor R1;
+    DcMotor L2;
+    DcMotor R2;
 
     @Override
     public void runOpMode() {
-        Left = hardwareMap.dcMotor.get("LeftMotor");
-        Right = hardwareMap.dcMotor.get("RightMotor");
+        L1 = hardwareMap.dcMotor.get("L1");
+        R1 = hardwareMap.dcMotor.get("R1");
+        L2 = hardwareMap.dcMotor.get("L2");
+        R2 = hardwareMap.dcMotor.get("R2");
+
 
         waitForStart();
 
@@ -29,8 +36,10 @@ public class Robot extends LinearOpMode {
             double leftPower = -gamepad1.left_stick_y;
             double rightPower = -gamepad1.right_stick_y;
 
-            Left.setPower(Range.clip((leftPower),-1,1));
-            Right.setPower(Range.clip((rightPower),-1,1));
+            L1.setPower(Range.clip((leftPower),-1,1));
+            R1.setPower(Range.clip((rightPower),-1,1));
+            L2.setPower(Range.clip((leftPower),-1,1));
+            R2.setPower(Range.clip((rightPower),-1,1));
 
 
             idle();
